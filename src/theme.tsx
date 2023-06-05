fc4574e5028c9b36ed0f4743e3d38a1781f4475c
Daehyeon Lee <31493850+DaehyeonLee@users.noreply.github.com>
2023-06-05 18:45:27 +0900

    commit

---
+++ src/theme.tsx
@@ -0,0 +1,23 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Theme = () => {
+  return (
+    <Wrapper>
+      <Title>hello</Title>
+    </Wrapper>
+  );
+};
+export default Theme;
